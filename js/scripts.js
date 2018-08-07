8aa6f2680f130a1221af4787bbb0e489a7d5df41
Akjol Jaenbai and Brian Nelson <brian@>
2018-08-07 09:37:57 -0700

    add feature to toggle hidden column when clicking on text.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $(".clickable.head-click").click(function() {
+    $(".head").toggle();
+  });
+  $(".clickable.chest-click").click(function() {
+    $(".chest").toggle();
+  });
+  $(".clickable.leg-click").click(function() {
+    $(".legs").toggle();
+  });
+
+
+});
